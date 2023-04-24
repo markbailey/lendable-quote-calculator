f13682f341e88443493af030f2c57046fe48db10
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-24 17:10:48 +0100

    Update calculateRepayments.ts

--- src/utilities/calculateRepayments.ts
+++ src/utilities/calculateRepayments.ts
@@ -5,11 +5,13 @@ function calculateRepayments(loanAmount: number, loanTerm: number, interestRates
       ?.percent ?? 0;
 
   const monthlyInterestRate = interestRate / 100 / 12;
-  const pow = 1 / Math.pow(1 + monthlyInterestRate, loanTerm);
-  const monthlyInterest = monthlyInterestRate / (1 - pow);
-  const monthlyRepayment = Math.round((monthlyInterest * loanAmount + Number.EPSILON) * 100) / 100;
+  const pow = Math.pow(1 + monthlyInterestRate, -loanTerm);
+  const monthlyRepayment = loanAmount * (monthlyInterestRate / (1 - pow));
 
-  return Object.freeze({ interestRate, monthlyRepayment });
+  return Object.freeze({
+    interestRate,
+    monthlyRepayment: Math.round(monthlyRepayment * 100) / 100,
+  });
 }
 
 export default calculateRepayments;
